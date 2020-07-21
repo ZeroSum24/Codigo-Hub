b50c4ac846c840cfb9f0bfd62b546782a23f5d9f
BernardChoo <bernardchoowk@gmail.com>
2020-07-21 16:16:39 +0800

    Spin Loader enlargement.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,23 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import s from './Loader.module.scss';
+import { Grid } from '@material-ui/core';
 
 class Loader extends React.Component {
-    static propTypes = {
-        size: PropTypes.number.isRequired
-    };
+	static propTypes = {
+		size: PropTypes.number.isRequired
+	};
 
-    static defaultProps = {
-        size: 21
-    };
+	static defaultProps = {
+		size: 120
+	};
 
-    render() {
-        return (
-            <div className={cx(s.root, this.props.className)}>
-                <i className="la la-spinner la-spin" style={{fontSize: this.props.size}}/>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<Grid
+				style={{
+					margin: '0',
+					position: 'absolute',
+					top: '45%'
+				}}
+				container
+			>
+				<div className={cx(s.root, this.props.className)}>
+					<i className="la la-spinner la-spin" style={{ fontSize: this.props.size }} />
+					<a style={{ fontSize: '36px' }}> Loading Còdigo </a>
+				</div>
+			</Grid>
+		);
+	}
 }
 
 export default Loader;
