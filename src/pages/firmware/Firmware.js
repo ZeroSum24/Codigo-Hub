f940a99731450388cb76f25ab4d541414b8b32f6
BernardChoo <bernardchoowk@gmail.com>
2020-07-22 20:18:34 +0800

    change button color and C++ code.

--- src/pages/firmware/Firmware.js
+++ src/pages/firmware/Firmware.js
@@ -16,11 +16,58 @@ class Firmware extends React.Component {
 
 	render() {
 		const codeString = `
-# Source Code
- 
-// The DatePicker works by supplying a date to bias towards,
-as well as a default timezone.
- (num) => num + 1
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
+  the result to set the pulse width modulation (PWM) of an output pin.
+  Also prints the results to the Serial Monitor.
+
+  The circuit:
+  - potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the analog pin.
+    side pins of the potentiometer go to +5V and ground
+  - LED connected from digital pin 9 to ground
+
+  created 29 Dec. 2008
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
+*/
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  // map it to the range of the analog out:
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  // change the analog out value:
+  analogWrite(analogOutPin, outputValue);
+
+  // print the results to the Serial Monitor:
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\t output = ");
+  Serial.println(outputValue);
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(2);
+}
 `;
 
 		return (
@@ -51,32 +98,38 @@ as well as a default timezone.
 							<br />
 							<Row>
 								{' '}
-								<Col md={3} />
-								<Col md="auto">
+								<Col sm={4} md={2} />
+								<Col sm="auto">
 									{' '}
-									<Button color="primary">Download </Button>
+									<Button style={{ width: '180px' }} color="info">
+										Download{' '}
+									</Button>
 								</Col>
-								<Col md={3} />
+								<Col sm={4} md={2} />
 							</Row>
 							<br />
 							<Row>
 								{' '}
-								<Col md={3} />
-								<Col md="auto">
+								<Col sm={4} md={2} />
+								<Col sm="auto">
 									{' '}
-									<Button color="primary">Deploy to Device </Button>
+									<Button style={{ width: '180px' }} color="primary">
+										Deploy to Device{' '}
+									</Button>
 								</Col>
-								<Col md={3} />
+								<Col sm={4} md={2} />
 							</Row>
 							<br />
 							<Row>
 								{' '}
-								<Col md={3} />
-								<Col md="auto">
+								<Col sm={4} md={2} />
+								<Col sm="auto" md="auto">
 									{' '}
-									<Button color="primary">Donate to DEV </Button>
+									<Button style={{ width: '180px' }} color="success">
+										Donate to DEV{' '}
+									</Button>
 								</Col>
-								<Col md={3} />
+								<Col sm={4} md={2} />
 							</Row>
 							<br />
 							<br />
