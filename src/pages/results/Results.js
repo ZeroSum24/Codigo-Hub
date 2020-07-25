4c2f5e6dcffb567238ca72084fa404f71053ee94
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2020-07-25 13:28:29 +0100

    Removed unneeded comments

--- src/pages/results/Results.js
+++ src/pages/results/Results.js
@@ -29,11 +29,13 @@ class Results extends React.PureComponent {
 			name: ''
 
 		};
+
 		this.state = {
 			users:[],
 			firmwares: [],
 			requests: []
 		};
+
 	}
 
 	render() {
@@ -46,44 +48,38 @@ class Results extends React.PureComponent {
 					<Row>
 						<Col xs={12} sm={12} md={2}>
 						<Card body outline color="primary">
-						{/*Bernard: Please make this so it displays number of firmwares programatically*/}
 							<CardTitle align="center">Firmware ({this.state.firmwares.length})</CardTitle>
 						</Card>
 						<br />
 						<Card body outline color="primary">
-						{/*Bernard: Please make this so it displays number of users programatically*/}
 							<CardTitle align="center">Users ({this.state.users.length})</CardTitle>
 						</Card>
 						<br />
 						<Card body outline color="primary">
-						{/*Bernard: Please Make this so it displays number of requests programatically*/ }
 							<CardTitle align="center">Requests ({this.state.requests.length})</CardTitle>
 						</Card>
 						<br />
 						</Col>
 						<Col xs={12} sm={12} md={9}>
 								<h1 className="page-title">Available Firmware<span className="fw-semi-bold"></span></h1>
-								{/*Bernard: Please make this so we can pass in a "Firmware" object and then they are made within a list view so it is scalable, look at how
-								deviceOverview is implemented and plase implement in the same way*/}
 								<Widget
-					        title={<h5>Firmware Name: <small className="text-muted">Firmware Name</small></h5>}
-					        close collapse >
-					        <p></p>
-					        <div className="widget-padding-md w-100 h-100 text-left border rounded">
-					          {/*TODO there is a bug with all the text being displayed as a single line, please display as a table*/}
-					          <Row>
-					            <Col sm={6}>
-					              <h6><span className="fw-semi-bold">Author: </span></h6>
-					              <h6><span className="fw-semi-bold">Version: </span></h6>
-					            </Col>
-					            <Col sm={6}>
-					              <h6>Author Name</h6>
-					              <h6>123456</h6>
-					            </Col>
-					          </Row>
-					        </div>
-					      </Widget>
-						</Col>
+									title={<h5>Firmware Name: <small className="text-muted">Firmware Name</small></h5>}
+									close collapse >
+									<p></p>
+									<div className="widget-padding-md w-100 h-100 text-left border rounded">
+										<Row>
+											<Col sm={6}>
+												<h6><span className="fw-semi-bold">Author: </span></h6>
+												<h6><span className="fw-semi-bold">Version: </span></h6>
+											</Col>
+											<Col sm={6}>
+												<h6>Author Name</h6>
+												<h6>123456</h6>
+											</Col>
+										</Row>
+									</div>
+								</Widget>
+								</Col>
 					</Row>
 					<Row />
 				</Container>
@@ -93,7 +89,6 @@ class Results extends React.PureComponent {
 }
 
 function DeviceListView() {
-
 
 }
 
