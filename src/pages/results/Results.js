6473bc534f9ac3b94a0458f9a8f82c2f7c2531e6
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2020-07-25 12:33:30 +0100

    Update to small part

--- src/pages/results/Results.js
+++ src/pages/results/Results.js
@@ -79,42 +79,6 @@ class Results extends React.PureComponent {
 					          </Row>
 					        </div>
 					      </Widget>
-								<Widget
-					        title={<h5>Firmware Name: <small className="text-muted">Firmware Name</small></h5>}
-					        close collapse >
-					        <p></p>
-					        <div className="widget-padding-md w-100 h-100 text-left border rounded">
-					          {/*TODO there is a bug with all the text being displayed as a single line, please display as a table*/}
-					          <Row>
-					            <Col sm={6}>
-					              <h6><span className="fw-semi-bold">Author: </span></h6>
-					              <h6><span className="fw-semi-bold">Version: </span></h6>
-					            </Col>
-					            <Col sm={6}>
-					              <h6>Author Name</h6>
-					              <h6>123456</h6>
-					            </Col>
-					          </Row>
-					        </div>
-					      </Widget>
-								<Widget
-					        title={<h5>Firmware Name: <small className="text-muted">Firmware Name</small></h5>}
-					        close collapse >
-					        <p></p>
-					        <div className="widget-padding-md w-100 h-100 text-left border rounded">
-					          {/*TODO there is a bug with all the text being displayed as a single line, please display as a table*/}
-					          <Row>
-					            <Col sm={6}>
-					              <h6><span className="fw-semi-bold">Author: </span></h6>
-					              <h6><span className="fw-semi-bold">Version: </span></h6>
-					            </Col>
-					            <Col sm={6}>
-					              <h6>Author Name</h6>
-					              <h6>123456</h6>
-					            </Col>
-					          </Row>
-					        </div>
-					      </Widget>
 						</Col>
 					</Row>
 					<Row />
