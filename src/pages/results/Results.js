4a04df810f985b5e5daedcf9af258819bb948a51
BernardChoo <bernardchoowk@gmail.com>
2020-07-26 14:21:27 +0800

    minor fixes on alignment.

--- src/pages/results/Results.js
+++ src/pages/results/Results.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import {connect} from "react-redux";
-import FirmwareWidget from './components/firmwareWidget'
-
+import { connect } from 'react-redux';
+import FirmwareWidget from './components/firmwareWidget';
 
 import { Row, Col, Container, Card, CardTitle, CardText, CardImg, CardBody, Button } from 'reactstrap';
 
 class Results extends React.PureComponent {
-
-
 	constructor(props) {
 		super(props);
 		const users = {
-  	name: 'Ross',
+			name: 'Ross'
 		};
 
 		this.state = {
-			users:[],
+			users: [],
 			firmwares: [
-				{id:'name', value: 'Name'},
-				{id:'author', value: 'author'},
-				{id:'version', value: 'version'}
+				{ id: 'name', value: 'Name' },
+				{ id: 'author', value: 'author' },
+				{ id: 'version', value: 'version' }
 			],
 			requests: []
 		};
-
 	}
 
 	render() {
@@ -34,26 +30,25 @@ class Results extends React.PureComponent {
 				</h1>
 				<Container fluid={true}>
 					<Row>
+						{' '}
 						<Col xs={12} sm={12} md={2}>
-						<Card body outline color="primary">
-							<CardTitle align="center">Firmware ({this.state.firmwares.length})</CardTitle>
-						</Card>
-						<br />
-						<Card body outline color="primary">
-							<CardTitle align="center">Users ({this.state.users.length})</CardTitle>
-						</Card>
-						<br />
-						<Card body outline color="primary">
-							<CardTitle align="center">Requests ({this.state.requests.length})</CardTitle>
-						</Card>
-						<br />
+							<Card body outline color="primary">
+								<CardTitle align="center">Firmware ({this.state.firmwares.length})</CardTitle>
+							</Card>
+							<br />
+							<Card body outline color="primary">
+								<CardTitle align="center">Users ({this.state.users.length})</CardTitle>
+							</Card>
+							<br />
+							<Card body outline color="primary">
+								<CardTitle align="center">Requests ({this.state.requests.length})</CardTitle>
+							</Card>
+							<br />
 						</Col>
-						<Col xs={12} sm={12} md={9}>
-								<h1 className="page-title">Available Firmware<span className="fw-semi-bold"></span></h1>
-								      <FirmwareListView firmwares={this.state.firmwares}/>
-								</Col>
+						<Col xs={12} sm={12} md={10}>
+							<FirmwareListView firmwares={this.state.firmwares} />
+						</Col>
 					</Row>
-					<Row />
 				</Container>
 			</div>
 		);
@@ -61,13 +56,13 @@ class Results extends React.PureComponent {
 }
 
 function FirmwareListView(props) {
-    let view;
-    if (props.firmwares.length > 0) {
-      		view = (props.firmwares.map((item) => <FirmwareWidget firmware={item}/>));
-    } else {
-      view = (<div align ="center">Sorry, no firmware has been found, why not propose a bounty?</div>);
-    }
-  return view
+	let view;
+	if (props.firmwares.length > 0) {
+		view = props.firmwares.map((item) => <FirmwareWidget firmware={item} />);
+	} else {
+		view = <div align="center">Sorry, no firmware has been found, why not propose a bounty?</div>;
+	}
+	return view;
 }
 
 export default Results;
