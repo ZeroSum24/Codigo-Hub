e7a787b3dd5c51f434898b1c15175c3a1c4c290a
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2020-07-28 17:03:38 +0100

    User profile centered image

--- src/pages/userProfile/userProfile.js
+++ src/pages/userProfile/userProfile.js
@@ -6,8 +6,9 @@ import {Grid} from "@material-ui/core";
 import {Button} from "reactstrap";
 import {Container, Alert, FormGroup, InputGroup, Input, Label} from 'reactstrap';
 import 'react-toastify/dist/ReactToastify.css';
-
+import logo from '../../images/rsz_4rsz_codigo-01.png';
 import Widget from '../../components/Widget';
+import ExampleComponent from "react-rounded-image";
 import Bounty from '../../model/Bounty'
 
 class UserProfile extends React.Component {
@@ -16,9 +17,16 @@ class UserProfile extends React.Component {
     return (
       <Container>
         <Widget className="widget-auth mx-auto" title={<h3 className="mt-0"></h3>}>
-          <p className="widget-auth-info">
-            Please fill all fields below.
-          </p>
+          <div align="center">
+            <ExampleComponent
+              image={logo}
+              roundedColor=""
+              imageWidth="150"
+              imageHeight="150"
+              roundedSize="13"
+           />
+
+          </div>
           <form onSubmit={this.handleAddBounty}>{
               this.props.errorMessage && (
                 <Alert className="alert-sm widget-middle-overflow rounded-0" color="danger">
