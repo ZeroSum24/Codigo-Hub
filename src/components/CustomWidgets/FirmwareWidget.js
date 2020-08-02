e8e19842993fdd66fc772a6ef80e96eb41f9f600
Mahbub Iftekhar <s1539308@sms.ed.ac.uk>
2020-08-02 06:27:04 +0100

    Reworking firmware

--- src/components/CustomWidgets/FirmwareWidget.js
+++ src/components/CustomWidgets/FirmwareWidget.js
@@ -25,23 +25,47 @@ class FirmwareWidget extends React.PureComponent {
   render() {
     const firmware = this.props.item;
     return (
+      /*
+      <h6><span className="fw-semi-bold">Developer: </span></h6>
+
+
+      <h6>{firmware.developer}</h6>
+      <h6>{firmware.version}</h6>
+      <h6>{firmware.description}</h6>
+      <h6>{firmware.device_type}</h6>
+
+      */
+
       <Widget
-        title={<h5>Firmware Name: <small className="text-muted">{firmware.name}</small></h5>}
         close collapse onClick={this.openFirmwareView}>
         <div className="widget-padding-md w-100 h-100 text-left border rounded">
+        <Row>
+          <Col>
+          <h3>{firmware.name}</h3>
+          </Col>
+          <Col>
+
+          </Col>
+        </Row>
+
           <Row>
-            <Col sm={6}>
-              <h6><span className="fw-semi-bold">Developer: </span></h6>
-              <h6><span className="fw-semi-bold">Version: </span></h6>
-              <h6><span className="fw-semi-bold">Description: </span></h6>
-              <h6><span className="fw-semi-bold">Device Type: </span></h6>
+            <Col>
+              <Row>
+                <h4><span className="fw-semi-bold">{firmware.device_type}</span></h4>
+              </Row>
             </Col>
-            <Col sm={6}>
-              <h6>{firmware.developer}</h6>
-              <h6>{firmware.version}</h6>
-              <h6>{firmware.description}</h6>
-              <h6>{firmware.device_type}</h6>
+
+            <Col>
+              <Row>
+                <h4><span className="fw-semi-bold">"  " + {firmware.version}</span></h4>
+              </Row>
             </Col>
+
+
+          </Row>
+
+          <Row>
+            <h4><span className="fw-semi-bold">{firmware.developer}</span></h4>
           </Row>
         </div>
       </Widget>
